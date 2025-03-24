96b1a841484b28b2fa56b8bd327257f03bc68074
XavierLaforgue <xavierjimmys@gmail.com>
2025-03-24 15:33:23 +0100

    Create custom main and move Holberton test main to test folder

---
+++ my_main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main: tests my_printf
+ * Return: always 0
+ */
+
+int main(void)
+{
+	unsigned int my_bytes, std_bytes;
+
+	my_bytes = my_printf("Hell%c %s", 'o', "World");
+	std_bytes = printf("Hell%c %s", 'o', "World");
+
+	printf("my_bytes = %ud", my_bytes);
+
+	return (0);
+}
